7657c2ba35100f74a477f6da9b2562fd0fbaaf12
Egemen-dev0 <keskinegemen0@gmail.com>
2024-02-15 09:21:37 +0300

    Gonderi gözükmsi sağlandı

--- src/App.js
+++ src/App.js
@@ -5,35 +5,37 @@
 */
 
 // State hook u import edin
-import React from "react";
+import React, {useState} from "react";
+
 
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
 import "./App.css";
-
+import Posts from "./bilesenler/Gonderiler/Gonderiler"
+import ac from "./bilesenler/AramaCubugu/AramaCubugu"
+import sahteVeri from "./sahte-veri"  
+import AramaCubugu from "./bilesenler/AramaCubugu/AramaCubugu";
 const App = () => {
-  // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
-  // Artık sahteVeri'ye ihtiyacınız olmayacak.
-  // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
+  
+let [gonderiNesneDizisi, setgonderiNesneDizisi] = useState(sahteVeri)
+let [aramaCubuguInput, setaramaCubuguInput] = useState("")
+
+const aramayıPasla = (text) => {
+setaramaCubuguInput(text)
+// let aramaSonucu = gonderiNesneDizisi.filter((item) =>  text)
+// setgonderiNesneDizisi(aramaSonucu); ==> BURAYI UPDATE!
+}
 
   const gonderiyiBegen = (gonderiID) => {
-    /*
-      Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
+    gonderiNesneDizisi.map((currentPost) => currentPost.id === gonderiID && currentPost.likes++ )
+    setgonderiNesneDizisi(gonderiNesneDizisi)
 
-      Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
-      Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
-
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
-      `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
-        - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
-        - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
-     */
   };
 
   return (
     <div className="App">
-      App Çalışıyor
-      {/* Yukarıdaki metni projeye başladığınızda silin*/}
+      <AramaCubugu />
+      <Posts acquiredData ={gonderiNesneDizisi}/>
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
--- src/bilesenler/Gonderiler/Gonderiler.js
+++ src/bilesenler/Gonderiler/Gonderiler.js
@@ -4,10 +4,14 @@ import "./Gonderiler.css";
 
 const Gonderiler = (props) => {
   // 🔥 Gönderiler'in ebeveyninin doğru değişkenleri doğru şekilde ilettiğine emin olun!
-  const { gonderiyiBegen, gonderiler } = props;
+  const { gonderiyiBegen, acquiredData } = props;
 
   return (
     <div className="posts-container-wrapper">
+{acquiredData.map((currentPost) => 
+<Gonderi key={currentPost.id} gonderi={currentPost} /> /*indivuadual posts will be sent*/
+)}
+
       {/* gönderiler dizisini işleyip her döngüde bir Gönderi bileşeni çağırın*/}
       {/* Gönderi'nin çağırılmasında hangi propları kullanmanız gerektiğine dikkat edin! */}
     </div>
